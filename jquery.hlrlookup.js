6be1725c34e965d050bd3c1a7b543b156d963379
Callum Macgregor <callum.macgregor@3gtelecoms.net>
2015-04-23 12:50:43 +0100

    first commit

---
+++ jquery.hlrlookup.js
@@ -0,0 +1,41 @@
+(function($) {     
+
+    $.fn.hlrlookup = function( options ) {
+        
+        var settings = $.extend({
+            complete     : null,
+            error        : null,
+            password     : '',
+            apikey       : '',
+            msisdninput  : ''
+        }, options);
+
+        return this.each( function() {
+            
+            $(this).submit( function(e) {
+                
+                e.preventDefault();
+                
+                 var msisdn = $(this).find(settings.msisdninput).val();
+                 
+                $.ajax( {
+                    type: 'GET',
+                    url: 'https://www.hlrlookup.com/api/hlr/?apikey='+settings.apikey+'&password='+settings.password+'&msisdn='+msisdn,
+                    success: function (msg) {
+                        var json = JSON.parse(msg);
+                        if ( json.error_code == 0 ) {
+                            settings.complete.call(this,"live");
+                        } else {
+                            settings.complete.call(this,"not live");
+                        }
+                    },
+                    error: function(msg) {
+                        settings.error.call(this,msg);
+                    }
+                })
+            }); 
+        }); 
+
+    }
+
+}(jQuery));
